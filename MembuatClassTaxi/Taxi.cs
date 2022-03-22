f191984895353191cd44054c41df065bb8ffc8f3
Vinzzztty <kevinarnandes21@students.amikom.ac.id>
2022-03-22 16:13:42 +0700

    first commit

---
+++ MembuatClassTaxi/Taxi.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MembuatClassTaxi
+{
+    class Taxi
+    {
+        //Variabel Properties Name, Bool, int
+        //Variabel method TaxiInfo, PickUpPassenger, DropOffPassenger
+        
+        //Properties
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        //method
+        public void InfoDriver()
+        {
+            Console.WriteLine("Driver name: {0}", DriverName);
+            Console.WriteLine("On Duty: {0}", OnDuty);
+            Console.WriteLine("Number of Passenger: {0}", NumPassenger);
+            Console.WriteLine();
+        }
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} sedang menjemput penumpang", DriverName);
+        }
+
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} selesai mengantar penumpang", DriverName);
+        }
+    }
+}
